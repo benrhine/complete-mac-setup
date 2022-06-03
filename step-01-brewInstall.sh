cb11b1b49371e1fad14578d3c28c5d8f0a0f0681
Ben Rhine <rhine.ben@gmail.com>
2022-06-02 20:00:38 -0600

    Update step-01-brewInstall.sh
    adding in command to link brew install

--- step-01-brewInstall.sh
+++ step-01-brewInstall.sh
@@ -8,6 +8,9 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Once install has been verified, update homebrew recipes
 brew update
 
